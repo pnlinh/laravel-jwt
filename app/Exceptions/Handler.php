5423d8f1562d8d9f59dd6243a1fb87e15e537703
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-17 23:38:12 +0700

    handle jwt

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +50,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenInvalidException) {
+            return response()->json(['error' => 'Token is invalid'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($exception instanceof TokenExpiredException) {
+            return response()->json(['error' => 'Token is expired'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($exception instanceof JWTException) {
+            return response()->json(['error' => 'There is problem with token'], Response::HTTP_BAD_REQUEST);
+        }
+
         return parent::render($request, $exception);
     }
 }
